b61c649b08affc7a16ba53bc8e4dfdd059142318
Sathya Moorthy K <ritish1808@gmail.com>
2026-08-04 09:36:26 +0530

    add uploads

--- server/server.ts
+++ server/server.ts
@@ -12,11 +12,14 @@ import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import Tesseract from 'tesseract.js';
 import { parseResume } from './src/services/resumeParser.ts';
+import dns from "dns";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -25,9 +28,6 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  socketOptions: {
-    family: 4, // 👈 Type-safe way to force IPv4
   },
 } as nodemailer.TransportOptions);
 
